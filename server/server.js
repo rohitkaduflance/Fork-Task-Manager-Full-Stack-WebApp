6d9ed38ec83c2b62de43a5fa60167267e964532d
rohitkaduflance <74869857+rohitkaduflance@users.noreply.github.com>
2024-08-18 02:07:37 +0530

    For Deployment of Render

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ import express from "express";
 import cors from "cors";
 import tasks from "./routes/tasks.js";
 
+//for deployment on render - next 2 lines
+const path = require("path");
+app.use(express.static(path.join(__dirname, "build"))); // put this line of code in app.js
+
 const PORT = process.env.PORT || 5050;
 const app = express();
 
